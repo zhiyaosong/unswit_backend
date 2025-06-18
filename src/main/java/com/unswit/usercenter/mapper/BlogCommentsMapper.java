0f61f719ed0ce6589f1bcce5e3f63732f75736e8
Zhiyao Song <song_zhiyao@163.com>
2025-06-18 22:42:34 +1000

    修复bug

---
+++ src/main/java/com/unswit/usercenter/mapper/BlogCommentsMapper.java
@@ -0,0 +1,18 @@
+package com.unswit.usercenter.mapper;
+
+import com.unswit.usercenter.model.domain.BlogComments;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author zhiyao
+* @description 针对表【blog_comments(blog comments)】的数据库操作Mapper
+* @createDate 2025-06-12 12:02:11
+* @Entity com.unswit.usercenter.model.domain.BlogComments
+*/
+public interface BlogCommentsMapper extends BaseMapper<BlogComments> {
+
+}
+
+
+
+
---
+++ src/main/java/com/unswit/usercenter/mapper/BlogMapper.java
@@ -0,0 +1,18 @@
+package com.unswit.usercenter.mapper;
+
+import com.unswit.usercenter.model.domain.Blog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author zhiyao
+* @description 针对表【blog(帖子)】的数据库操作Mapper
+* @createDate 2025-06-12 12:02:11
+* @Entity com.unswit.usercenter.model.domain.Blog
+*/
+public interface BlogMapper extends BaseMapper<Blog> {
+
+}
+
+
+
+
